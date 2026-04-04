279e5f8d39c74706353aee5d3d37133f541136a3
raul zambrano <raulzambranoleon15@gmail.com>
2026-04-04 16:47:15 -0500

    feat: notificaciones toast

--- src/hooks/useCrudPage.js
+++ src/hooks/useCrudPage.js
@@ -1,22 +1,35 @@
 import { useCrud } from "./useCrud";
 import { useForm } from "./useForm";
 import { useModal } from "./useModal";
+import toast from "react-hot-toast";
 
 export function useCrudPage({ service, formularioInicial }) {
-    const { data, loading, crear, actualizar, eliminar } = useCrud(service);
+    const { data, loading, crear, actualizar, eliminar: eliminarRegistro } = useCrud(service);
     const { show, ocultarModal, mostrarModal } = useModal();
     const { formulario, setFormulario, limpiarFormulario, editando, setEditando } = useForm({ formularioInicial });
+    const eliminar =  (id) => {
+        try {
+            eliminarRegistro(id);
+            toast.success("Registro eliminado correctamente");
+        } catch (error) {
+            console.log(error);
+            toast.error("Hubo un error al eliminar", error);
+        }
+    }
     const guardar = async () => {
         try {
             if (editando) {
                 await actualizar(formulario.id, formulario);
+                toast.success("Registro actualizado correctamente");
             } else {
                 await crear(formulario);
+                toast.success("Registro creado correctamente");
             }
             limpiarFormulario();
             ocultarModal();
         } catch (error) {
             console.error("Hubo un error al guardar", error);
+            toast.error("Hubo un error al guardar", error);
         }
     }
     return {
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,11 @@
 import { Container } from "react-bootstrap";
 import { NavbarMantenimiento } from "@/components/NavbasMantenimiento";
 import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 export function Layout() {
     return (
         <>
+            <Toaster position="bottom-right" reverseOrder={false} toastOptions={{ duration: 3000 }} />
             <NavbarMantenimiento />
             <Container className="mt-4">
                 <Outlet />
